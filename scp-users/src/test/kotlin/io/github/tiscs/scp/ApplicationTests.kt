1ba3c069fc61ea348d456ebd18b02a57009028d2
Tiscs <tiscs@live.com>
2019-03-17 00:09:21 +0800

    Set logging level to debug for Exposed when dev profile is active;

--- scp-users/src/test/kotlin/io/github/tiscs/scp/ApplicationTests.kt
+++ scp-users/src/test/kotlin/io/github/tiscs/scp/ApplicationTests.kt
@@ -1,10 +1,14 @@
 package io.github.tiscs.scp
 
+import io.github.tiscs.scp.models.Clients
+import io.github.tiscs.scp.models.Users
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.transaction.annotation.Transactional
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -12,5 +16,11 @@ import org.springframework.test.context.junit4.SpringRunner
 class ApplicationTests {
     @Test
     fun contextLoads() {
+    }
+
+    @Test
+    @Transactional
+    fun exposedCreate() {
+        SchemaUtils.create(Users, Clients)
     }
 }
