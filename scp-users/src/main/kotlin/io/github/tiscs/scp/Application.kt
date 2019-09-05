505817fd8a8b38af4a068a686cd4782377e18ad2
Tiscs <tiscs@live.com>
2019-09-05 15:05:58 +0800

    Disable redis session store in dev or test profile;

--- scp-users/src/main/kotlin/io/github/tiscs/scp/Application.kt
+++ scp-users/src/main/kotlin/io/github/tiscs/scp/Application.kt
@@ -2,10 +2,8 @@ package io.github.tiscs.scp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @SpringBootApplication
-@EnableRedisHttpSession
 class Application
 
 fun main(args: Array<String>) {
