7d5eca311611c1a009eef0152f656446d260f72f
Tiscs <tiscs@live.com>
2020-01-05 00:06:02 +0800

    Update dockerfile;

--- scp-users/src/main/kotlin/io/github/tiscs/scp/controllers/UtilsController.kt
+++ scp-users/src/main/kotlin/io/github/tiscs/scp/controllers/UtilsController.kt
@@ -1,21 +1,30 @@
 package io.github.tiscs.scp.controllers
 
+import io.github.tiscs.scp.models.Event
 import io.github.tiscs.scp.snowflake.IdWorker
 import io.swagger.annotations.Api
+import org.slf4j.LoggerFactory
+import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.messaging.Source
 import org.springframework.http.ResponseEntity
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.time.Clock
+import java.time.LocalDateTime
+import java.util.*
 
 @Api(tags = ["Utils"])
+@EnableBinding(value = [Source::class])
 @RestController
-@RequestMapping(method = [RequestMethod.POST], path = ["/utils"], consumes = ["application/x-www-form-urlencoded"])
+@RequestMapping(method = [RequestMethod.POST], path = ["/utils"])
 class UtilsController(
         private val idWorker: IdWorker,
+        private val eventSource: Source,
         private val passwordEncoder: PasswordEncoder
 ) {
+    private val logger = LoggerFactory.getLogger(UtilsController::class.java)
+
     @RequestMapping(path = ["/snowflake/long"])
     fun nextLong(): ResponseEntity<Long> {
         return ResponseEntity.ok(idWorker.nextLong())
@@ -31,8 +40,24 @@ class UtilsController(
         return ResponseEntity.ok(idWorker.nextBase64())
     }
 
-    @RequestMapping(path = ["/password/encode"])
+    @RequestMapping(path = ["/datetime/utc"])
+    fun datetimeUTCNow(): ResponseEntity<String> {
+        return ResponseEntity.ok(LocalDateTime.now(Clock.systemUTC()).toString())
+    }
+
+    @RequestMapping(path = ["/datetime/local"])
+    fun datetimeLocalNow(): ResponseEntity<String> {
+        logger.debug(TimeZone.getDefault().id)
+        return ResponseEntity.ok(LocalDateTime.now().toString())
+    }
+
+    @RequestMapping(path = ["/password/encode"], consumes = ["application/x-www-form-urlencoded"])
     fun encodePassword(@RequestParam password: String): ResponseEntity<String> {
         return ResponseEntity.ok(passwordEncoder.encode(password))
+    }
+
+    @RequestMapping(path = ["/events/publish"], consumes = ["application/json"])
+    fun publishEvent(@RequestBody event: Event<Any>): ResponseEntity<Boolean> {
+        return ResponseEntity.ok(eventSource.output().send(MessageBuilder.withPayload(event).build()))
     }
 }
