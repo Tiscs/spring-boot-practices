8c2186b9e573d0bfd9d01a0c65700890a566601c
Tiscs <tiscs@live.com>
2022-12-13 17:10:51 +0800

    Add native compile support;

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.0")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.1")
+    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.19")
 }
