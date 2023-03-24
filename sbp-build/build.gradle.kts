235aa33efcc293c8e306a45b1eafffe35aea1272
Tiscs <tiscs@live.com>
2023-03-24 14:19:48 +0800

    Upgrade to spring boot 3.0.5;

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.10")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.4")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.5")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.2")
     implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.20")
