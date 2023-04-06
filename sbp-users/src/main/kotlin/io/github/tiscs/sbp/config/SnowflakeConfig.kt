3fd3b88332e2fdb4f4452f9caefa224bed2bf6da
Tiscs <tiscs@live.com>
2023-04-06 10:45:24 +0800

    Upgrade to Kotlin 1.8.20;

--- sbp-users/src/main/kotlin/io/github/tiscs/sbp/config/SnowflakeConfig.kt
+++ sbp-users/src/main/kotlin/io/github/tiscs/sbp/config/SnowflakeConfig.kt
@@ -3,6 +3,7 @@ package io.github.tiscs.sbp.config
 import io.github.tiscs.sbp.sequence.RedisSequenceProvider
 import io.github.tiscs.sbp.snowflake.ConfigProperties
 import io.github.tiscs.sbp.snowflake.IdWorker
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -15,12 +16,20 @@ import java.util.*
 @ConditionalOnProperty("snowflake.enabled", matchIfMissing = false)
 @EnableConfigurationProperties(ConfigProperties::class)
 class SnowflakeConfig {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(SnowflakeConfig::class.java)
+    }
+
     @Bean
     fun idWorker(config: ConfigProperties, factory: Optional<RedisConnectionFactory>): IdWorker {
         val workerId = if (!config.workerSequence.isNullOrEmpty()) {
             Assert.state(factory.isPresent, "Redis connection is required when generating worker id in sequence.")
-            RedisSequenceProvider(config.workerSequence, factory.get(), 0, (-1L shl config.workerIdBits).inv()).nextValue()
-        } else config.workerId
+            RedisSequenceProvider(config.workerSequence, factory.get(), 0, (-1L shl config.workerIdBits).inv()).nextValue().also {
+                LOGGER.info("Load worker id from redis sequence: {}", config.workerSequence)
+            }
+        } else config.workerId.also {
+            LOGGER.info("Use configured worker id: {}", config.workerId)
+        }
         return IdWorker(config.clusterId, workerId, config.clusterIdBits, config.workerIdBits, config.sequenceBits, config.idEpoch)
     }
 }
