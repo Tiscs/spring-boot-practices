8e09ea40938b845f2331c698ce48876fe83db564
Tiscs <tiscs@live.com>
2023-09-24 14:04:30 +0800

    Bump spring boot from 3.1.3 to 3.1.4;

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.10")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.3")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.4")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.3")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
     implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.27")
