8373aebf60802c55380ea281bcac9b159a7e257b
Tiscs <tiscs@live.com>
2023-11-01 22:55:43 +0800

    Bump kotlin from 1.9.10 to 1.9.20;

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -7,8 +7,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.20")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.5")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.3")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
