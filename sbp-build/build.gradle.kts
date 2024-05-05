ca0ed40f032d3e22e752a0bb8673556f9b7b3494
Tiscs <tiscs@live.com>
2024-05-05 11:29:30 +0800

    Bump io.spring.gradle:dependency-management-plugin from 1.1.4 to 1.1.5

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.23")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.2.5")
-    implementation("io.spring.gradle:dependency-management-plugin:1.1.4")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.5")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
     implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.10.1")
 }
