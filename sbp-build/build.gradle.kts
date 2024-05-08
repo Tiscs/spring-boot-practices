6fd4829d348b775adb9c413da265873377e742b0
Tiscs <tiscs@live.com>
2024-05-08 22:05:40 +0800

    Upgrade to Gradle 8.7, Kotlin 2.0.0;

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -7,10 +7,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.24")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:2.0.0")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.2.5")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.5")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
-    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.10.1")
+    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.10.2")
 }
