7a390fd69e276ac72927955e1cf5d8f3a777aaf4
Tiscs <tiscs@live.com>
2024-08-23 11:58:53 +0800

    Bump kotlin from 2.0.0 to 2.0.20;

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -7,9 +7,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:2.0.0")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.3.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:2.0.20")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.3.3")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.6")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
     implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.10.2")
