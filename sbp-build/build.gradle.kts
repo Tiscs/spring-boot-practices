065285fc8731b143f4984c89aca0f293b63c3d1a
Tiscs <tiscs@live.com>
2025-08-01 11:31:03 +0800

    Bump spring boot from 3.5.3 to 3.5.4;

--- sbp-build/build.gradle.kts
+++ sbp-build/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     implementation("org.jetbrains.kotlin:kotlin-allopen:2.2.0")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.5.3")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.5.4")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.5")
     implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.11.0")
